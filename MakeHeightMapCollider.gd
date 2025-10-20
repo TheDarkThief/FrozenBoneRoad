ab9a0df2fe6d61b8d569b7cc625465a726dd7c2e
TheDarkThief <thedarkthief@mailfence.com>
2025-10-19 23:06:52 -0700

    Fixed terrain, made blend scene to make terrain generation easier

--- MakeHeightMapCollider.gd
+++ MakeHeightMapCollider.gd
@@ -1,8 +1,8 @@
-# I just duplicated your script since none of the tutorials I found worked oops
+# Fixed the script so it can work with EXRs
 
 @tool
 extends CollisionShape3D
-@export var TEST_HEIGHTMAP: CompressedTexture2D = preload("uid://2s2ok0iykl4n")
+@export var TEST_HEIGHTMAP: CompressedTexture2D
 @export var MAX_HEIGHT = 65:
 	set(newHeight):
 		MAX_HEIGHT = newHeight
@@ -13,14 +13,13 @@ extends CollisionShape3D
 
 func _on_height_set():
 	var heightMapFormatted = TEST_HEIGHTMAP.get_image()
+	heightMapFormatted.decompress()
 	heightMapFormatted.convert(Image.FORMAT_RF)
 	var heightmap_collider = HeightMapShape3D.new()
 	heightmap_collider.map_width = WIDTH
 	heightmap_collider.map_depth = DEPTH
 	heightmap_collider.update_map_data_from_image(heightMapFormatted, 0, MAX_HEIGHT)
-	
-	
-	
+		
 	shape = heightmap_collider
 
 	
