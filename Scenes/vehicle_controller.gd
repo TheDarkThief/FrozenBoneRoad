174e986d4c5a5549c6cbf1f3373a54d1d9caef95
TheDarkThief <thedarkthief@mailfence.com>
2025-10-24 01:50:08 -0700

    Made vehicle follow terrain TODO:Vehicle controller and internal collisions

--- Scenes/vehicle_controller.gd
+++ Scenes/vehicle_controller.gd
@@ -6,15 +6,29 @@ extends CharacterBody3D
 
 @onready var vehicle_model: Node3D = $TheVehicle
 
+@onready var front_ray_pos: CSGSphere3D = $FrontRayPos
+@onready var calculated_pos_viz: CSGSphere3D = $CalculatedPosViz
+
 var isPlayerControlling := false
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	
+	front_ray_pos.position = front_cast.get_collision_point() - position
 	
 	var left_vec = left_wheel_cast.get_collision_point() - front_cast.get_collision_point()
 	var right_vec = right_wheel_cast.get_collision_point() - front_cast.get_collision_point()
 	
-	var up_vector = left_vec.cross(right_vec).normalized()
-	var point_dir = up_vector.cross(transform.basis.z).normalized() * -1
+	var midpoint_pos = front_cast.get_collision_point() + left_wheel_cast.get_collision_point() + right_wheel_cast.get_collision_point()
+	midpoint_pos = midpoint_pos * 0.333333333333
+	midpoint_pos = to_local(midpoint_pos)
 	
-	vehicle_model.transform = vehicle_model.transform.looking_at(point_dir, Vector3(0,1,0), true)
+	calculated_pos_viz.position = midpoint_pos
+	
+	var up_vector = left_vec.cross(right_vec).normalized()
+	var point_dir = up_vector.cross(transform.basis.x.normalized()).normalized()
+	
+	vehicle_model.position = Vector3(0,0,0)
+	vehicle_model.transform = vehicle_model.transform.looking_at(point_dir, up_vector, false)
+	vehicle_model.position = midpoint_pos
+	
+	
